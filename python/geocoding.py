ae731a81e17e8bf67cfbea2c998a7665a4f968ac
Michael Yi <wooseok@uw.edu>
2017-10-14 16:55:59 -0400

    geocoding address

--- python/geocoding.py
+++ python/geocoding.py
@@ -1,1 +1,7 @@
 import googlemaps
+
+gmaps = googlemaps.Client(key='AIzaSyAGQh741SvVZndL19xOcQx-fVY9YuuEjuQ')
+
+# geocoding an address
+geocoded_address = gmaps.geocode('315 NE Campus Pkwy, Seattle, WA ')
+print(geocoded_address)
