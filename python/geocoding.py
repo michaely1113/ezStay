4f11eb231c610fbf9191a06b02b4a1eb05d464e2
Michael Yi <wooseok@uw.edu>
2017-10-14 18:55:44 -0400

    working on geocode

--- python/geocoding.py
+++ python/geocoding.py
@@ -1,22 +1,25 @@
 from tokens import GOOGLEMAPS_KEY
-import scraper
+#import scraper
 import googlemaps
+import json
 
 gmaps = googlemaps.Client(key=GOOGLEMAPS_KEY)
 
 # geocoding an address sample
-geocoded_address = gmaps.geocode('315 NE Campus Pkwy, Seattle, WA ')
+geocoded_address = gmaps.geocode('315 NE Campus Pkwy, Seattle, WA')
 print(geocoded_address)
 
 
 # now, we need to be able to get a list of <addresses from tyler> then convert it to a list of geocoded addresses
-addressesOfEvents = scraper.getAddressesOfEvents()
+#addressesOfEvents = scraper.getAddressesOfEvents()   #TODO COMMENT OUT NEXT THREE LINES
+addressesOfEvents = []
+addressesOfEvents.append('315 NE Campus Pkwy, Seattle, WA')
+addressesOfEvents.append('5015 16th Ave NE, Seattle, WA')
 geocodedEvents = []
 for address in addressesOfEvents:
     geocodedEvents.append(gmaps.geocode(address))
 
-
-
-
+for geocodedEvent in geocodedEvents:
+    print(geocodedEvent['geometry'])
 
 # get geocoded addresses, run algo where we find midpoint between all points
