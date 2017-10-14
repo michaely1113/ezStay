31bfa27372b45c060c36dede89712f57cea426b7
Michael Yi <wooseok@uw.edu>
2017-10-14 19:45:07 -0400

    finished geocoding

--- python/geocoding.py
+++ python/geocoding.py
@@ -2,7 +2,9 @@ from tokens import GOOGLEMAPS_KEY
 #import scraper
 import googlemaps
 import json
+import requests
 
+GMAPS_GEOCODE_API_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'
 gmaps = googlemaps.Client(key=GOOGLEMAPS_KEY)
 
 # geocoding an address sample
@@ -19,7 +21,33 @@ geocodedEvents = []
 for address in addressesOfEvents:
     geocodedEvents.append(gmaps.geocode(address))
 
-for geocodedEvent in geocodedEvents:
-    print(geocodedEvent['geometry'])
+
+#################### - SAMPLE HTTP REQUEST - ####################
+# sample http request
+url = GMAPS_GEOCODE_API_ENDPOINT % (addressesOfEvents[0], GOOGLEMAPS_KEY)
+response = json.loads(requests.get(url).text)
+results = response['results']
+response = results[0]
+#print(response)
+#print(response['geometry']['location'])
+#################### - SAMPLE HTTP REQUEST - ####################
+
+
 
 # get geocoded addresses, run algo where we find midpoint between all points
+locations = []
+for geocodedEvent in geocodedEvents:
+    locations.append(geocodedEvent[0]['geometry']['location'])
+lat = 0
+lng = 0
+for location in locations:
+    lat += location['lat']
+    print(location['lat'])
+    lng += location['lng']
+    print(location['lng'])
+averageLat = lat / locations.__len__()
+averageLng = lng / locations.__len__()
+
+#find address
+midpointAddress = gmaps.reverse_geocode((averageLat, averageLng))
+print(midpointAddress[0]['formatted_address'])
