1a7a4e3c7f4c2565fa31d559038baea5a0f98799
Michael Yi <wooseok@uw.edu>
2017-10-14 23:52:24 -0400

    added front-end

--- python/flightinfo.py
+++ python/flightinfo.py
@@ -1,16 +1,17 @@
 import json
 import urllib.request as request
 
-def getFlightInfo():
-	return request.urlopen("https://demo30-test.apigee.net/v1/hack/status?flightNumber=1500&flightOriginDate=2016-09-25&apikey=ZINxBqol4GEAB9L1T25ZcFyG9vmapoLW").read()
+# 4 digit flightNumber
+# date in YYYY-MM-DD format
+def getFlightInfo(flightNumber, date):
+    API_endpoint = "https://demo30-test.apigee.net/v1/hack/status?flightNumber=%s&flightOriginDate=%s&apikey=ZINxBqol4GEAB9L1T25ZcFyG9vmapoLW"
+    url = API_endpoint % (flightNumber, date)
+    return json.loads(request.urlopen(url).read())
 
-response = json.loads(getFlightInfo())
-arrivalCity = response['flightStatusResponse']['statusResponse']['flightStatusTO']['flightStatusLegTOList']["arrivalCityName"]
-arrivalTime = response['flightStatusResponse']['statusResponse']['flightStatusTO']['flightStatusLegTOList']["arrivalLocalTimeEstimatedActual"]
+def getDestination(flightNumber, date):
+    response = getFlightInfo(flightNumber, date)
+    return response['flightStatusResponse']['statusResponse']['flightStatusTO']['flightStatusLegTOList']["arrivalCityName"]
 
-
-def getDestination():
-    return arrivalCity
-
-def getArrivalTime():
-    return arrivalTime
+def getArrivalTime(flightNumber, date):
+    response = getFlightInfo(flightNumber, date)
+    return response['flightStatusResponse']['statusResponse']['flightStatusTO']['flightStatusLegTOList']['arrivalLocalTimeScheduled']
